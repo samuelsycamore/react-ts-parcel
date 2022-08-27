45bcf0f152726c262be0bcfd5937317f46ad3530
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-08-27 15:25:34 -0500

    initializing

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { render } from "react-dom";
+ 
+const App = () => {
+  return (
+    <div>
+      <p>React / TypeScript / Parcel starter</p>
+    </div>
+  );
+};
+ 
+render(<App />, document.getElementById("app"));
